7938dbc6b15e51f55fa9bac722bfbf664fc8c5b2
Matthieu MARTIN <codingstuff91@gmail.com>
2020-06-18 10:21:11 +0200

    Edit style of groupTransition component

--- src/components/GroupTransition.vue
+++ src/components/GroupTransition.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="container">
     <h1>{{ msg }}</h1>
-    <input type="text" placeholder="Add a fruit to the list" v-model="newFruit" @keydown.enter="addFruit">
-    <button @click="addFruit">Add a new fuit to the list</button>
-    <button @click="sortFruits">Sort the fruits</button>
-    <button @click="showText = !showText">Show the text</button>
+
+    <div class="row">
+      <input type="text" placeholder="Add a fruit to the list" v-model="newFruit" @keydown.enter="addFruit">
+      <button @click="addFruit">Add a new fuit to the list</button>
+    </div>
+
+    <div class="row">
+      <button @click="sortFruits">Sort the fruits</button>
+      <button @click="showText = !showText">Show the text</button>
+    </div>
+
     <div>
       <transition-group name="slide-up" tag="ul" appear>
         <li v-for="(fruit,index) in fruits" :key="index">{{fruit}}</li>
       </transition-group>
     </div>
+
     <div>
       <transition
       @before-enter="beforeEnter"
@@ -26,7 +34,7 @@
 import Velocity from 'velocity-animate'
 
 export default {
-  name: 'HelloWorld',
+  name: 'GroupTransition',
   data() {
     return {
       fruits : ["pommes","poires","Fraises","Kiwis"],
@@ -51,7 +59,7 @@ export default {
     enter(el, done){
       Velocity(el,
       {opacity : 1},
-      {duration : 4000, easing : 'easeOutCubic', complete : done})
+      {duration : 1000, easing : 'easeOutCubic', complete : done})
     }
   },
 }
@@ -59,11 +67,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.row{
+  margin-top : 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+}
+button{
+  margin-left: 10px;
+  padding : 5px;
+}
 h3 {
   margin: 40px 0 0;
 }
 li{
   margin-top: 10px;
+  list-style-type: none;
 }
 
 /* Etat initial de la transition*/
